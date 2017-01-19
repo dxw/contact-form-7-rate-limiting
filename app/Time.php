f77a9c851a1e6441f28d6093273aac70b878c303
Tom Adams <tom@dxw.com>
2017-01-19 14:29:30 -0500

    Add Time->secondsAgo()

--- app/Time.php
+++ app/Time.php
@@ -6,6 +6,16 @@ class Time
 {
     public function now()
     {
-        return gmstrftime('%Y-%m-%dT%H:%M:%S');
+        return $this->getTimestamp(time());
+    }
+
+    public function secondsAgo(int $seconds)
+    {
+        return $this->getTimestamp(time()-$seconds);
+    }
+
+    private function getTimestamp(int $time)
+    {
+        return gmstrftime('%Y-%m-%dT%H:%M:%S', $time);
     }
 }
--- spec/time.spec.php
+++ spec/time.spec.php
@@ -4,6 +4,7 @@ describe(\Dxw\ContactForm7RateLimiting\Time::class, function () {
     beforeEach(function () {
         \WP_Mock::setUp();
 
+        \phpmock\mockery\PHPMockery::mock(\Dxw\ContactForm7RateLimiting::class, "time")->andReturn(1000);
         $this->time = new \Dxw\ContactForm7RateLimiting\Time();
     });
 
@@ -14,9 +15,17 @@ describe(\Dxw\ContactForm7RateLimiting\Time::class, function () {
 
     describe('->now()', function () {
         it('returns the current time', function () {
-            \phpmock\mockery\PHPMockery::mock(\Dxw\ContactForm7RateLimiting::class, "gmstrftime")->andReturn('2017-01-19T11:27:59');
+            \phpmock\mockery\PHPMockery::mock(\Dxw\ContactForm7RateLimiting::class, "gmstrftime")->with('%Y-%m-%dT%H:%M:%S', 1000)->andReturn('2017-01-19T11:27:59');
 
             expect($this->time->now())->to->equal('2017-01-19T11:27:59');
+        });
+    });
+
+    describe('->secondsAgo()', function () {
+        it('returns the time 100 seconds ago', function () {
+            \phpmock\mockery\PHPMockery::mock(\Dxw\ContactForm7RateLimiting::class, "gmstrftime")->with('%Y-%m-%dT%H:%M:%S', 900)->andReturn('2017-01-19T11:27:59');
+
+            expect($this->time->secondsAgo(100))->to->equal('2017-01-19T11:27:59');
         });
     });
 });
